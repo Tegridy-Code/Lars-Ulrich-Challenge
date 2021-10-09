2a6c65ff1970b2ded8d496bbee05d3d05591d894
Alex <asigalov61@hotmail.com>
2021-10-09 14:33:20 -0700

    Version 1.0 Added MIDI Events Pass-Through Option
    Now it sounds so much better :)
    
    Also, added unzip for the model
    
    Added environment setup
    
    Updated notes/descriptions

--- Lars_Ulrich_Challenge.py
+++ Lars_Ulrich_Challenge.py
@@ -25,10 +25,24 @@
 # 
 # ***
 
+# # Setup Environment
+
+# ## Clone repo and install dependencies
+
 # In[ ]:
 
 
-#@title Import all needed modules
+get_ipython().system('git clone https://github.com/Tegridy-Code/Lars-Ulrich-Challenge')
+    
+get_ipython().system('pip install torch')
+get_ipython().system('pip install tqdm')
+get_ipython().system('pip install matplotlib')
+
+
+# ## Import all needed modules
+
+# In[ ]:
+
 
 print('Loading needed modules. Please wait...')
 import os
@@ -37,6 +51,8 @@ import secrets
 import copy
 import tqdm
 from tqdm import auto
+
+os.chdir('Lars-Ulrich-Challenge')
 
 print('Loading TMIDIX module...')
 import TMIDIX
@@ -60,22 +76,31 @@ print('Done!')
 # In[ ]:
 
 
-#print('Loading MIDI file...')
+print('Lars Ulrich Challenge Algorithmic Drums Generator')
+print('Project Los Angeles')
+print('Tegridy Code 2021')
 
 source_MIDI_file = 'Nothing Else Matters.mid'
 number_of_drums_per_chord = 3 # drums pitches per chord/note
+pass_through_MIDI = True # Pass-through or not all MIDI events
 
+# print('Loading MIDI file...')
 mel_crd_f = []
 
 score = TMIDIX.midi2ms_score(open(source_MIDI_file, 'rb').read())
 
+all_events_matrix = []
 events_matrix = []
 itrack = 1
 
 while itrack < len(score):
     for event in score[itrack]:
+        
         if event[0] == 'note' and event[3] != 9:
             events_matrix.append(event)
+            
+        all_events_matrix.append(event)
+        
     itrack += 1
     
 # print('Grouping by start time. This will take a while...')
@@ -114,23 +139,67 @@ for i in tqdm(range(len(src_pitches))):
         # print('not in list')
         pass
     final_song.extend(mel_crd_f[i])
+    
     if idx != -1:
         for d in drums[idx][:number_of_drums_per_chord]:
             # if d < 40:
             drum = [['note', mel_crd_f[i][0][1], mel_crd_f[i][0][2], 9, d, 100 ]]
             final_song.extend(drum)
             drums_map.extend(drum)
+            
         idx = -1
-        
-TMIDIX.Tegridy_SONG_to_MIDI_Converter(final_song, 
-                                      output_file_name='./LUC-Algorithmic-Composition',
-                                      output_signature = 'Project Los Angeles', 
-                                      track_name = 'Lars Ulrich Challenge',
-                                      number_of_ticks_per_quarter=500,
-                                      list_of_MIDI_patches = [0, 24, 32, 40, 42, 46, 56, 71, 73, 0, 0, 0, 0, 0, 0, 0])
+
+if pass_through_MIDI:
+    
+    output =  all_events_matrix + drums_map
+    
+    output.sort(key=lambda x: x[1])
+    
+    midi_data = TMIDIX.score2midi([score[0], output,
+                                    [['track_name', 0, bytes('Project Los Angeles', 'utf-8')]],
+                                    [['track_name', 0, bytes('Lars Ulrich Challenge', 'utf-8')]]])
+
+    detailed_MIDI_stats = TMIDIX.score2stats([score[0], output,
+                                                [['track_name', 0, bytes('Project Los Angeles', 'utf-8')]],
+                                                [['track_name', 0, bytes('Lars Ulrich Challenge', 'utf-8')]]])
+
+    with open('./LUC-Algorithmic-Composition' + '.mid', 'wb') as midi_file:
+        midi_file.write(midi_data)
+        midi_file.close()
+    
+    print('Done! Enjoy! :)')
+    
+    print(detailed_MIDI_stats)
+    
+else:             
+    TMIDIX.Tegridy_SONG_to_MIDI_Converter(final_song, 
+                                          output_file_name='./LUC-Algorithmic-Composition',
+                                          output_signature = 'Project Los Angeles', 
+                                          track_name = 'Lars Ulrich Challenge',
+                                          number_of_ticks_per_quarter=500,
+                                          list_of_MIDI_patches = [0, 24, 32, 40, 42, 46, 56, 71, 73, 0, 0, 0, 0, 0, 0, 0])
 
 
 # # Artificial Intelligence Drums Matching
+
+# ## Unzip the Model
+
+# In[ ]:
+
+
+print('=' * 70)
+print('Unzipping the model...Please wait...')
+print('=' * 70)
+
+get_ipython().system('cat Clean-MIDI-Transformer-Model.zip* > Clean-MIDI-Transformer-Model.zip')
+print('=' * 70)
+
+get_ipython().system('unzip -j Clean-MIDI-Transformer-Model.zip')
+print('=' * 70)
+
+print('Done! Enjoy! :)')
+print('=' * 70)
+
 
 # ## Load Clean MIDI Transformer Model
 
@@ -162,24 +231,32 @@ print('Done!')
 # In[ ]:
 
 
-#print('Loading MIDI file...')
+print('Lars Ulrich Challenge Model Generator')
+print('Project Los Angeles')
+print('Tegridy Code 2021')
 
 source_MIDI_file = 'Nothing Else Matters.mid' # soutce MIDI file
 number_of_drums_per_chord = -1 # drums pitches per chord/note
 memory_tokens = 64 # recommended 32-64
+pass_through_MIDI = True # Pass-through or not all MIDI events
 
-
+# print('Loading MIDI file...')
 
 mel_crd_f = []
 score = TMIDIX.midi2ms_score(open(source_MIDI_file, 'rb').read())
 
+all_events_matrix = []
 events_matrix = []
 itrack = 1
 
 while itrack < len(score):
     for event in score[itrack]:
+        
         if event[0] == 'note' and event[3] != 9: # reading all notes events except for the drums
             events_matrix.append(event)
+            
+        all_events_matrix.append(event)
+        
     itrack += 1
     
 # print('Grouping by start time. This will take a while...')
@@ -209,6 +286,9 @@ for m in mel_crd_f:
 final_song = []
 out = [0]
 
+# print('Generating drums...')
+drums_map = []
+
 for i in tqdm(range(len(src_pitches))):
 #for i in tqdm([0]):
     
@@ -228,11 +308,39 @@ for i in tqdm(range(len(src_pitches))):
     for d in out_cc[:number_of_drums_per_chord]:
         drum = [['note', mel_crd_f[i][0][1], mel_crd_f[i][0][2], 9, d, 100 ]]
         final_song.extend(drum)
+        drums_map.extend(drum)
         
-TMIDIX.Tegridy_SONG_to_MIDI_Converter(final_song, 
-                                      output_file_name='./LUC-Artificial-Intelligence-Composition',
-                                      output_signature = 'Project Los Angeles',
-                                      track_name = 'Lars Ulrich Challenge',
-                                      number_of_ticks_per_quarter=500,
-                                      list_of_MIDI_patches = [0, 24, 32, 40, 42, 46, 56, 71, 73, 0, 0, 0, 0, 0, 0, 0])
+print('Writing MIDI...')
 
+if pass_through_MIDI:
+    
+    output =  all_events_matrix + drums_map
+    
+    output.sort(key=lambda x: x[1])
+    
+    midi_data = TMIDIX.score2midi([score[0], output,
+                                    [['track_name', 0, bytes('Project Los Angeles', 'utf-8')]],
+                                    [['track_name', 0, bytes('Lars Ulrich Challenge', 'utf-8')]]])
+
+    detailed_MIDI_stats = TMIDIX.score2stats([score[0], output,
+                                                [['track_name', 0, bytes('Project Los Angeles', 'utf-8')]],
+                                                [['track_name', 0, bytes('Lars Ulrich Challenge', 'utf-8')]]])
+    
+    with open('./LUC-Artificial-Intelligence-Composition' + '.mid', 'wb') as midi_file:
+        midi_file.write(midi_data)
+        midi_file.close()
+    
+    print('Done! Enjoy! :)')
+    
+    print(detailed_MIDI_stats)
+    
+else:    
+    TMIDIX.Tegridy_SONG_to_MIDI_Converter(final_song, 
+                                          output_file_name='./LUC-Artificial-Intelligence-Composition',
+                                          output_signature = 'Project Los Angeles',
+                                          track_name = 'Lars Ulrich Challenge',
+                                          number_of_ticks_per_quarter=500,
+                                          list_of_MIDI_patches = [0, 24, 32, 40, 42, 46, 56, 71, 73, 0, 0, 0, 0, 0, 0, 0])
+
+
+# # Congrats! You did it! :)
